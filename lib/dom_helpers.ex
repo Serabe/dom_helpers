a76fec1fc69fc98c28e8f60bbd2f0d1bb1dc758c
Sergio Arbeo <serabe@gmail.com>
2023-10-03 02:01:42 +0200

    Improve docs

--- lib/dom_helpers.ex
+++ lib/dom_helpers.ex
@@ -1,6 +1,10 @@
 defmodule DomHelpers do
   @moduledoc """
   Just `use DomHelpers` to import all the methods.
+
+  For more information about accessor helpers, see `DomHelpers.Accessors`.
+
+  For more information about selection helpers, see `DomHelpers.Selectors`.
   """
 
   defmacro __using__(_opts) do
